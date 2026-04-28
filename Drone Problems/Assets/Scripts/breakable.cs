5fcedb870a67854e81387d653d07834ed1bcc3ed
Prince Patel <156550398+IMPr1nce@users.noreply.github.com>
2026-04-28 12:22:31 -0500

    added the destructable wall prefab need to use it

---
+++ Drone Problems/Assets/Scripts/breakable.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class breakable : MonoBehaviour
+{
+    [SerializeField] private GameObject brokenVersionPrefab;
+    [SerializeField] private GameObject notBrokenVersionPrefab;
+
+    private BoxCollider bc;
+
+    private void Awake()
+    {
+        bc = GetComponent<BoxCollider>();
+
+        if (notBrokenVersionPrefab != null)
+            notBrokenVersionPrefab.SetActive(true);
+
+        if (brokenVersionPrefab != null)
+            brokenVersionPrefab.SetActive(false);
+
+        if (bc != null)
+            bc.enabled = true;
+    }
+
+    private void Break()
+    {
+        if (notBrokenVersionPrefab != null)
+            notBrokenVersionPrefab.SetActive(false);
+
+        if (brokenVersionPrefab != null)
+            brokenVersionPrefab.SetActive(true);
+
+        if (bc != null)
+            bc.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Break();
+        }
+    }
+}
